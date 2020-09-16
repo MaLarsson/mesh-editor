62deeee176c522455ad5eacae97d7ccf9ffd9a07
Marcus Larsson <marcuslarsson_92@hotmail.com>
2020-09-16 21:13:22 +0200

    added glad as the opengl loader for the project

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
+#define GLFW_INCLUDE_NONE
+
 #include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
 #include <iostream>
 
 static void ErrorCallback(int error, const char* description) {
@@ -16,19 +20,36 @@ int main() {
 
   if (!glfwInit()) return 1;
 
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+
   GLFWwindow* window = glfwCreateWindow(640, 480, "Window Title", nullptr, nullptr);
 
   if (!window) {
+    std::cout << "Error: Failed to create window\n";
     glfwTerminate();
     return 1;
   }
 
   glfwSetKeyCallback(window, KeyCallback);
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Error: Failed to initialize OpenGL context\n";
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 1;
+  }
+
 
   while (!glfwWindowShouldClose(window)) {
+
+    // main loop ...
+
     glfwPollEvents();
   }
 
+  glfwDestroyWindow(window);
   glfwTerminate();
 
   return 0;
