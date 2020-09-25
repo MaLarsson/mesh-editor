6f2091f80b1ff7e07495b196bf02b6ba29828cc0
lin-b <yeelin.b@gmail.com>
2020-09-25 23:21:05 +0800

    added ifc parser

---
+++ tools/ifcparser.py
@@ -0,0 +1,36 @@
+IfcVersion = "IFC4X1"
+IfcSchema = IfcVersion + ".exp";
+
+# private globals
+_Type = "TYPE"
+_Entity = "ENTITY"
+_EndEntity = "END_ENTITY"
+
+# TYPE: REAL, INTEGER, STRING, ENUMERATION OF
+# ENTITY: SUBTYPE OF
+
+def parse_ifc_schema(filename):
+	with open(filename, 'r') as file:
+		curr_block = ""
+		for idx, line in enumerate(file):
+			if line.startswith(_Type):
+				parse_ifc_type(line)
+			elif line.startswith(_Entity):
+				curr_block += line
+			elif line.startswith(_EndEntity):
+				print(curr_block)
+				parse_ifc_entity(curr_block)
+				curr_block = ""
+			else:
+				curr_block += line
+			# if idx > 1000: break
+
+def parse_ifc_type(string):
+	return None
+	#print(string)
+
+def parse_ifc_entity(string):
+	return None
+	#print(string)
+
+parse_ifc_schema(IfcSchema)
