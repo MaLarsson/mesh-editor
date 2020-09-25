c2dd01741788b3ad5401188487a0ce23c230eeb6
lin-b <yeelin.b@gmail.com>
2020-09-25 23:25:51 +0800

    change tabs to spaces

--- tools/ifcparser.py
+++ tools/ifcparser.py
@@ -10,27 +10,25 @@ _EndEntity = "END_ENTITY"
 # ENTITY: SUBTYPE OF
 
 def parse_ifc_schema(filename):
-	with open(filename, 'r') as file:
-		curr_block = ""
-		for idx, line in enumerate(file):
-			if line.startswith(_Type):
-				parse_ifc_type(line)
-			elif line.startswith(_Entity):
-				curr_block += line
-			elif line.startswith(_EndEntity):
-				print(curr_block)
-				parse_ifc_entity(curr_block)
-				curr_block = ""
-			else:
-				curr_block += line
-			# if idx > 1000: break
+    with open(filename, 'r') as file:
+        curr_block = ""
+        for idx, line in enumerate(file):
+            if line.startswith(_Type):
+                parse_ifc_type(line)
+            elif line.startswith(_Entity):
+                curr_block += line
+            elif line.startswith(_EndEntity):
+                parse_ifc_entity(curr_block)
+                curr_block = ""
+            else:
+                curr_block += line
 
 def parse_ifc_type(string):
-	return None
-	#print(string)
+    return None
+    #print(string)
 
 def parse_ifc_entity(string):
-	return None
-	#print(string)
+    return None
+    #print(string)
 
 parse_ifc_schema(IfcSchema)
